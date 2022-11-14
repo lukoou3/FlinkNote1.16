45b2f5f7c71ed7f2a0d21d715533ef1ffa2921b8
lifengchao5 <lifengchao5@jd.com>
2022-11-14 22:46:40 +0800

    toDataStream

--- src/main/java/com/flink/sql/dsintegration/HandleInsertOnlyStreamSuite.scala
+++ src/main/java/com/flink/sql/dsintegration/HandleInsertOnlyStreamSuite.scala
@@ -7,9 +7,11 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.table.api.bridge.scala._
 import HandleInsertOnlyStreamSuite._
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.table.api.Schema
+import org.apache.flink.table.api.{DataTypes, Schema}
 import org.apache.flink.table.api.bridge.scala.internal.StreamTableEnvironmentImpl
-import org.apache.flink.table.data.RowData
+import org.apache.flink.table.data.conversion.DataStructureConverters
+import org.apache.flink.table.data.{GenericRowData, RowData, StringData}
+import org.apache.flink.table.runtime.typeutils.{ExternalTypeInfo, InternalTypeInfo}
 import org.apache.flink.table.types.DataType
 import org.apache.flink.table.types.utils.TypeInfoDataTypeConverter
 import org.apache.flink.types.Row
@@ -172,8 +174,305 @@ class HandleInsertOnlyStreamSuite extends FlinkBaseSuite{
     }
   }
 
+  test("ds转table_fromDataStream_DataStream[RowData]2"){
+    val names = Array("莫南","璇音","青丝","流沙")
+
+    val rowDataDs: DataStream[RowData] = env.fromSequence(1, 10000).map{ i =>
+      if(i % 5 > 0){
+        val name = names((i % 4).toInt)
+        val age = java.lang.Integer.valueOf((18 + i % 100).toInt)
+        val cnt = java.lang.Long.valueOf(18 + i % 100)
+        val row: RowData = GenericRowData.of(StringData.fromString(name), age, cnt)
+        row
+      }else{
+        GenericRowData.of(null, null, null)
+      }
+    }(ExternalTypeInfo.of(
+      DataTypes.ROW(
+        DataTypes.FIELD("name", DataTypes.STRING()),
+        DataTypes.FIELD("age", DataTypes.INT()),
+        DataTypes.FIELD("cnt", DataTypes.BIGINT())
+      ).bridgedTo(classOf[RowData])
+    ))
+
+    println((ExternalTypeInfo.of(
+      DataTypes.ROW(
+        DataTypes.FIELD("name", DataTypes.STRING()),
+        DataTypes.FIELD("age", DataTypes.INT()),
+        DataTypes.FIELD("cnt", DataTypes.BIGINT())
+      ).bridgedTo(classOf[RowData])
+    )))
+    val typeInformation: TypeInformation[RowData] = ExternalTypeInfo.of(
+      tEnv.asInstanceOf[StreamTableEnvironmentImpl].getCatalogManager.getDataTypeFactory
+        .createDataType("ROW<name string, age int, cnt bigint>").bridgedTo(classOf[RowData]))
+    println(typeInformation)
+
+    /**
+     * [[org.apache.flink.table.runtime.operators.source.InputConversionOperator]]
+     * internalRecord = converter.toInternal(externalRecord)
+     * [[org.apache.flink.table.data.conversion.IdentityConverter]]
+     */
+    val rstTable = tEnv.fromDataStream(rowDataDs)
+
+
+    val rowDataDataType: DataType = rstTable.getResolvedSchema.toPhysicalRowDataType.bridgedTo(classOf[RowData])
+    val rowDataDs2: DataStream[RowData] = rstTable.toDataStream[RowData](rowDataDataType)
+    //val rowDataDs: DataStream[RowData] = table.toAppendStream[RowData]
+    println(rowDataDs2.dataType)
+
+    rowDataDs2.addSink{ row =>
+      println(row)
+    }
+  }
+
+  test("ds转table_fromDataStream_DataStream[RowData]3"){
+    val names = Array("莫南","璇音","青丝","流沙")
+
+    val rowDataDs: DataStream[RowData] = env.fromSequence(1, 10000).map{ i =>
+      Thread.sleep(500)
+      if(i % 5 > 0){
+        val name = names((i % 4).toInt)
+        val age = java.lang.Integer.valueOf((18 + i % 100).toInt)
+        val cnt = java.lang.Long.valueOf(18 + i % 100)
+        val row: RowData = GenericRowData.of(StringData.fromString(name), age, cnt)
+        row
+      }else{
+        GenericRowData.of(null, null, null)
+      }
+    }(ExternalTypeInfo.of(
+      tEnv.asInstanceOf[StreamTableEnvironmentImpl].getCatalogManager.getDataTypeFactory
+        .createDataType("ROW<name string, age int, cnt bigint>").bridgedTo(classOf[RowData]))
+    )
+
+    /**
+     * [[org.apache.flink.table.runtime.operators.source.InputConversionOperator]]
+     * internalRecord = converter.toInternal(externalRecord)
+     * [[org.apache.flink.table.data.conversion.IdentityConverter]]
+     */
+    val rstTable = tEnv.fromDataStream(rowDataDs)
+
+
+    val rowDataDataType: DataType = rstTable.getResolvedSchema.toPhysicalRowDataType.bridgedTo(classOf[RowData])
+    val rowDataDs2: DataStream[RowData] = rstTable.toDataStream[RowData](rowDataDataType)
+    //val rowDataDs: DataStream[RowData] = table.toAppendStream[RowData]
+    println(rowDataDs2.dataType)
+
+    rowDataDs2.addSink{ row =>
+      println(row)
+    }
+  }
+
+  test("ds转table_fromDataStream_DataStream[RowData]4"){
+    val names = Array("莫南","璇音","青丝","流沙")
+
+    val rowDataDs: DataStream[RowData] = env.fromSequence(1, 10000).map{ i =>
+      if(i % 5 > 0){
+        val name = names((i % 4).toInt)
+        val age = java.lang.Integer.valueOf((18 + i % 100).toInt)
+        val cnt = java.lang.Long.valueOf(18 + i % 100)
+        val row: RowData = GenericRowData.of(StringData.fromString(name), age, cnt)
+        row
+      }else{
+        GenericRowData.of(null, null, null)
+      }
+    }(InternalTypeInfo.of[RowData](
+      tEnv.asInstanceOf[StreamTableEnvironmentImpl].getCatalogManager.getDataTypeFactory
+        .createDataType("ROW<name string, age int, cnt bigint>").getLogicalType)
+    )
+
+
+    /**
+     * [[org.apache.flink.table.runtime.operators.source.InputConversionOperator]]
+     * internalRecord = converter.toInternal(externalRecord)
+     * [[org.apache.flink.table.data.conversion.IdentityConverter]]
+     */
+    val rstTable = tEnv.fromDataStream(rowDataDs)
+
+
+    val rowDataDataType: DataType = rstTable.getResolvedSchema.toPhysicalRowDataType.bridgedTo(classOf[RowData])
+    val rowDataDs2: DataStream[RowData] = rstTable.toDataStream[RowData](rowDataDataType)
+    //val rowDataDs: DataStream[RowData] = table.toAppendStream[RowData]
+    val tape1 = InternalTypeInfo.of[RowData](
+      tEnv.asInstanceOf[StreamTableEnvironmentImpl].getCatalogManager.getDataTypeFactory
+        .createDataType("ROW<name string, age int, cnt bigint>").getLogicalType)
+    val tape2 = rowDataDs2.dataType
+    println(tape1)
+    println(tape2)
+    println()
+
+    rowDataDs2.addSink{ row =>
+      println(row)
+    }
+  }
+
+  test("table转ds_toDataStream_DataStream[Row]"){
+    val sql =
+      """
+    CREATE TABLE tmp_tb1 (
+      name string,
+      age int,
+      cnt bigint,
+      proctime as proctime()
+    ) WITH (
+      'connector' = 'faker',
+      'fields.name.expression' = '#{superhero.name}',
+      'fields.age.expression' = '#{number.numberBetween ''0'',''20''}',
+      'fields.cnt.expression' = '#{number.numberBetween ''0'',''20000000000''}',
+      'rows-per-second' = '1'
+    )
+    """
+    tEnv.executeSql(sql)
+
+    val table = tEnv.sqlQuery("select * from tmp_tb1")
+
+    val rowDs: DataStream[Row] = table.toDataStream
+    println(rowDs.dataType)
+
+    rowDs.addSink { row =>
+      println(row)
+    }
+  }
+
+  test("table转ds_toDataStream_DataStream[RowData]"){
+    val sql = """
+    CREATE TABLE tmp_tb1 (
+      name string,
+      age int,
+      cnt bigint,
+      data row<name string, age int>,
+      names array<string>,
+      datas array<row<name string, age int>>,
+      log_time timestamp(3),
+      log_time_ltz timestamp_ltz(3),
+      proctime as proctime()
+    ) WITH (
+      'connector' = 'faker',
+      'fields.name.expression' = '#{superhero.name}',
+      'fields.name.null-rate' = '0.2',
+      'fields.age.expression' = '#{number.numberBetween ''0'',''20''}',
+      'fields.age.null-rate' = '0.2',
+      'fields.cnt.expression' = '#{number.numberBetween ''0'',''20000000000''}',
+      'fields.cnt.null-rate' = '0.2',
+      'fields.data.name.expression' = '#{harry_potter.spell}',
+      'fields.data.age.expression' = '#{number.numberBetween ''20'',''30''}',
+      'fields.data.null-rate' = '0.3',
+      'fields.names.expression' = '#{harry_potter.spell}',
+      'fields.names.length' = '3',
+      'fields.names.null-rate' = '0.3',
+      'fields.datas.name.expression' = '#{harry_potter.spell}',
+      'fields.datas.age.expression' = '#{number.numberBetween ''20'',''30''}',
+      'fields.datas.length' = '3',
+      'fields.datas.null-rate' = '0.3',
+      'fields.log_time.expression' =  '#{date.past ''5'',''0'',''SECONDS''}',
+      'fields.log_time_ltz.expression' =  '#{date.past ''5'',''0'',''SECONDS''}',
+      'rows-per-second' = '1'
+    )
+    """
+    tEnv.executeSql(sql)
+
+    val table = tEnv.sqlQuery("select * from tmp_tb1")
+
+    /**
+     * 就是直接调用的： [[StreamTableEnvironment.toDataStream(Table)]]
+     * 实际实现功能的是：[[org.apache.flink.table.api.bridge.scala.internal.StreamTableEnvironmentImpl.toDataStream(Table)]]
+     * 然后调用的代码是：
+     * // include all columns of the query (incl. metadata and computed columns)
+     * val sourceType = table.getResolvedSchema.toSourceRowDataType
+     * toDataStream(table, sourceType)
+     * 没有bridged到RowData，所以生成的就是Row类型
+     */
+    val rowDs: DataStream[Row] = table.toDataStream
+    println(rowDs.dataType)
+
+
+    /**
+     * 转RowData，用新的方法需要显示绑定使用内部类型
+     * 使用toAppendStream[RowData]老的api则可以直接转为RowData。
+     */
+    val rowDataDataType: DataType = table.getResolvedSchema.toPhysicalRowDataType.bridgedTo(classOf[RowData])
+    val rowDataDs: DataStream[RowData] = table.toDataStream[RowData](rowDataDataType)
+    //val rowDataDs: DataStream[RowData] = table.toAppendStream[RowData]
+    println(rowDataDs.dataType)
+
+    rowDataDs.addSink { row =>
+      println(row)
+    }
+  }
+
+  test("table转ds_toDataStream_DataStream[CaseClass]"){
+    val sql = """
+    CREATE TABLE tmp_tb1 (
+      name string,
+      age int,
+      cnt bigint,
+      data row<name string, age int>,
+      names array<string>,
+      datas array<row<name string, age int>>,
+      log_time timestamp(3),
+      log_time_ltz timestamp_ltz(3),
+      proctime as proctime()
+    ) WITH (
+      'connector' = 'faker',
+      'fields.name.expression' = '#{superhero.name}',
+      'fields.name.null-rate' = '0.2',
+      'fields.age.expression' = '#{number.numberBetween ''0'',''20''}',
+       -- 'fields.age.null-rate' = '0.2',
+      'fields.cnt.expression' = '#{number.numberBetween ''0'',''20000000000''}',
+      'fields.cnt.null-rate' = '0.2',
+      'fields.data.name.expression' = '#{harry_potter.spell}',
+      'fields.data.age.expression' = '#{number.numberBetween ''20'',''30''}',
+      'fields.data.null-rate' = '0.3',
+      'fields.names.expression' = '#{harry_potter.spell}',
+      'fields.names.length' = '3',
+      'fields.names.null-rate' = '0.3',
+      'fields.datas.name.expression' = '#{harry_potter.spell}',
+      'fields.datas.age.expression' = '#{number.numberBetween ''20'',''30''}',
+      'fields.datas.length' = '3',
+      'fields.datas.null-rate' = '0.3',
+      'fields.log_time.expression' =  '#{date.past ''5'',''0'',''SECONDS''}',
+      'fields.log_time_ltz.expression' =  '#{date.past ''5'',''0'',''SECONDS''}',
+      'rows-per-second' = '1'
+    )
+    """
+    tEnv.executeSql(sql)
+
+    val table = tEnv.sqlQuery("select name,age,cnt,data,datas from tmp_tb1")
+
+    /**
+     * 把RowData转成外部类型
+     */
+    val dataType = DataTypes.of(classOf[TableCaseData]).toDataType(tEnv.asInstanceOf[StreamTableEnvironmentImpl].getCatalogManager.getDataTypeFactory)
+    println(dataType)
+    val structureConverter = DataStructureConverters.getConverter(dataType)
+    //new DataStructureConverterWrapper(DataStructureConverters.getConverter(dataType))
+
+    /**
+     * table转ds会加一个filter校验过滤非空属性，默认data中的非空属性table这行实际的列值为null时会抛出异常
+     * 设置'table.exec.sink.not-null-enforcer'='drop'后，会直接把这一行给直接过滤掉，肯定不能设置这个
+     * 这个是运行时报错的，要是实际就没null，转的data中使用原生类型也是不会报错的。不是编译阶段报错
+     * org.apache.flink.table.runtime.operators.sink.SinkNotNullEnforcer
+     *
+     * 实际相当于调用：toDataStream(table, DataTypes.of(targetClass))
+     */
+    val dataDs = table.toDataStream(classOf[TableCaseData])
+    println(dataDs.dataType)
+    //dataDs.print()
+    dataDs.addSink { row =>
+      println(row)
+    }
+  }
+
 }
 
 object HandleInsertOnlyStreamSuite{
   case class People(id: Long, name: String, age: Int, score: Double, event_time: Instant)
+
+  case class TableCaseSubData(name: String, age: Integer)
+
+  //case class TableCaseData(name: String, age: Integer, cnt: java.lang.Long, data: TableCaseSubData, datas:java.util.List[TableCaseSubData])
+  //case class TableCaseData(name: String, age: Integer, cnt: java.lang.Long, data: TableCaseSubData, datas:Array[TableCaseSubData])
+
+  case class TableCaseData(name: String, age: Int, cnt: java.lang.Long, data: TableCaseSubData, datas: Array[TableCaseSubData]){
+    println(111)
+  }
 }
