2bb26c60eb2cf155c1a8d572dbed0799d5d870ef
lifengchao5 <lifengchao5@jd.com>
2022-12-31 19:54:39 +0800

    RichOption

--- src/main/java/com/flink/stream/base/DataStreamWordCount.scala
+++ src/main/java/com/flink/stream/base/DataStreamWordCount.scala
@@ -2,14 +2,19 @@ package com.flink.stream.base
 
 // 和spark一样，需要引入隐式转换
 
+import org.apache.flink.configuration.DeploymentOptions
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
+
+import com.flink.utils.Converters._
 
 object DataStreamWordCount {
 
   def main(args: Array[String]): Unit = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    // local
+    println(env.getConfiguration.getOptional(DeploymentOptions.TARGET).asScala.getOrElse(""))
 
     // 需要先启动`nc -lk 9999`，用来发送数据，windows使用`nc -l -p 9999`命令
     val text: DataStream[String] = env.socketTextStream("localhost", 9999)
